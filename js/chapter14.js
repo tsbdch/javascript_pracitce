91b8836df30f3c4f86de67731aa45ee78434828f
tsbdch <tsuboi.daichi@fujitsu.com>
2020-06-16 21:38:41 +0900

    study class

--- js/chapter14.js
+++ js/chapter14.js
@@ -11,22 +11,95 @@ function factoryPerson(first, last) {
 function Person(first, last) {
   this.first = first;
   this.last = last;
-  this.introduce = function () {
-    console.log('object prototype');
-  }
 };
+
+// 冗長
+//  function Japanese(first, last) {
+//  this.last = last;
+//  this.doSado = function() {
+//    //茶道をする
+//    console.log('do 茶道');
+//  }
+//};
+
+//  call によるコンストラクタの継承
+function Japanese(first, last) {
+  Person.call(this, first, last);
+  this.lang = 'ja';
+}
+
+//  setPrototypeOf による prototype の継承
+Object.setPrototypeOf(Japanese.prototype, Person.prototype);
 
 Person.prototype.introduce = function () {
   console.log(`My name is ${this.first} ${this.last}`);
 }
 
+Japanese.prototype.sayJapanese = function() {
+  console.log(`こんにちは ${this.first} ${this.last}`)
+}
+
 let me = new Person('me', 'me');
 let me1 = new Person('me1', 'me1');
-
-me1.__proto__.introduce = function () {
-  console.log("I don't want to introduce myself");
-}
+let ja = new Japanese('j', 'p');
 
 me.introduce();
 me1.introduce();
+console.log(ja.lang);
+ja.introduce();
+ja.sayJapanese();
 
+//  class
+class PersonClass {
+  constructor(first, last){
+    this.first = first;
+    this.last = last;
+  }
+
+  introduce() {
+    console.log(`Hello ${this.first} ${this.last}`);
+  }
+
+  //  constructor から直接呼び出す場合は static
+  static sayHello(){
+    console.log('こんにちは sayHello');
+  }
+
+  //  getter, setter
+
+}
+
+//  継承
+class JapaneseClass extends PersonClass {
+  constructor(first, last){
+    super(first, last);
+    this.lang = 'ja';
+    this._age = 0;
+  }
+
+  sayJapanese() {
+    console.log('日本語')
+  }
+
+  // getter, setter
+  get age(){
+    return this._age;
+  }
+
+  set age(value){
+    this._age = value
+  }
+
+}
+
+let jaclass = new JapaneseClass('山田', '太郎');
+console.log(jaclass.lang);
+jaclass.sayJapanese();
+jaclass.introduce();
+JapaneseClass.sayHello();
+
+console.log(jaclass._age);
+console.log(jaclass.age);
+jaclass.age = 10;
+console.log(jaclass._age)
+console.log(jaclass.age)
